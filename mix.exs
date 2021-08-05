c2fe2663536f3817bfcab883473cb043a70c405d
Matvey Karpov <matveichk@gmail.com>
2021-08-05 11:45:32 +0700

    Added docs to hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule JViewer.MixProject do
       version: "0.1.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      name: "JViewer",
+      source_url: "https://github.com/mtvch/j-viewer",
+      package: package()
     ]
   end
 
@@ -20,6 +24,17 @@ defmodule JViewer.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description() do
+    "JViewer is an excellent way to declaratively represent elixir data in a JSON encodable format."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mtvch/j-viewer"}
     ]
   end
 end
